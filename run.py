d38131b1ba765ec25f9e0e93000cff33a0680b7c
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2020-03-10 04:28:51 +0300

    Added bind address and port options.

--- run.py
+++ run.py
@@ -53,6 +53,39 @@ def argHandler_terminalId(name):
 
     terminal_id = name
 
+def argHandler_listen(address):
+    arg_config["HOST_ADDRESS"] = address
+    config.HOST_ADDRESS = address
+
+def argHandler_port(port):
+    port = int(port)
+
+    arg_config["GOTTY_PORT"] = port
+    arg_config["HTTP_PORT"] = port+1
+    arg_config["SERVER_PORT"] = port+2
+
+    config.GOTTY_PORT = port
+    config.HTTP_PORT = port+1
+    config.SERVER_PORT = port+2
+
+def argHandler_httpPort(port):
+    port = int(port)
+
+    arg_config["HTTP_PORT"] = port
+    config.HTTP_PORT = port
+
+def argHandler_serverPort(port):
+    port = int(port)
+
+    arg_config["SERVER_PORT"] = port
+    config.SERVER_PORT = port
+
+def argHandler_gottyPort(port):
+    port = int(port)
+    
+    arg_config["GOTTY_PORT"] = port
+    config.GOTTY_PORT = port
+
 def argHandler_verbose():
     config.VERBOSE = True
     arg_config["VERBOSE"] = True
@@ -67,6 +100,11 @@ def argHandler_help():
     print("  --gdb-executable=PATH, -g PATH:\tSpecifies GDB executable path (Default is \"gdb\" command on PATH environment variable.)")
     print("  --tmux-executable=PATH, -tmux PATH:\tSpecifies Tmux executable path (Default is \"tmux\" command on PATH environment variable.)")
     print("  --terminal-id=NAME, -t NAME:\t\tSpecifies tmux terminal identifier name (Default is \"gdb-frontend\".)")
+    print("  --listen=IP, -l IP:\t\tSpecifies listen address for HTTP and WS servers.)")
+    print("  --port=PORT, -p PORT:\t\tSpecifies port range for three ports to (Gotty: PORT, HTTP: PORT+1, WS: PORT+2).)")
+    print("  --http-port=PORT:\t\tSpecifies HTTP server port.)")
+    print("  --server-port=PORT:\t\tSpecifies WS server port.)")
+    print("  --gotty-port=PORT:\t\tSpecifies Gotty server port.)")
     print("  --verbose, -V:\t\t\tEnables verbose output.")
     print("")
 
@@ -86,6 +124,11 @@ args = [
     ["--gdb-executable", "-g", argHandler_gdbExecutable, True],
     ["--tmux-executable", "-tmux", argHandler_tmuxExecutable, True],
     ["--terminal-id", "-t", argHandler_terminalId, True],
+    ["--listen", "-l", argHandler_listen, True],
+    ["--port", "-p", argHandler_port, True],
+    ["--http-port", False, argHandler_httpPort, True],
+    ["--server-port", "-p", argHandler_serverPort, True],
+    ["--gotty-port", "-p", argHandler_gottyPort, True],
     ["--help", "-h", argHandler_help, False],
     ["--version", "-v", argHandler_version, False]
 ]
@@ -120,7 +163,7 @@ for _user_arg in sys.argv[1:]:
                     _arg[2]()
 
                 break
-        elif _user_arg == _arg[1]:
+        elif _arg[1] and (_user_arg == _arg[1]):
             is_exists = True
 
             if _arg[3]:
@@ -137,7 +180,7 @@ for _user_arg in sys.argv[1:]:
         exit(0)
 
 if value_expected_arg:
-    print("Missing value for option:", value_expected_arg[1])
+    print("Missing value for option:", value_expected_arg[0] + (", " + value_expected_arg[1]) if value_expected_arg[1] else "")
     exit(0)
 
 if tmux_executable == "tmux" and not shutil.which("tmux"):
@@ -146,11 +189,20 @@ if tmux_executable == "tmux" and not shutil.which("tmux"):
 
 try:
     os.system(tmux_executable+" kill-session -t "+terminal_id)
-    os.system(tmux_executable+" -f tmux.conf new-session -s "+terminal_id+" -d '"+gdb_executable+" -ex \"python import sys, os; sys.path.insert(0, \\\""+path+"\\\"); import config, json, base64; config.init(); config.setJSON(base64.b64decode(\\\""+base64.b64encode(json.dumps(arg_config).encode()).decode()+"\\\").decode()); import main\"; read;'")
+
+    os.system(
+        tmux_executable +
+        " -f tmux.conf new-session -s " + terminal_id +
+        " -d '" + gdb_executable +
+        " -ex \"python import sys, os; sys.path.insert(0, \\\""+path+"\\\"); import config, json, base64; config.init(); " +
+        "config.setJSON(base64.b64decode(\\\""+base64.b64encode(json.dumps(arg_config).encode()).decode()+"\\\").decode()); import main\"; read;'"
+    )
+
     print("Listening on %s: http://127.0.0.1:%d/" % (config.HOST_ADDRESS, config.HTTP_PORT))
     print("|---------------------------------------------------------------------|")
     print(("| Open this address in web browser: \033[0;32;40mhttp://127.0.0.1:%d/terminal/\033[0m" % config.HTTP_PORT) + "   |")
     print("|---------------------------------------------------------------------|")
+
     os.system("./bin/gotty --config gotty.conf -a 127.0.0.1 -p "+str(config.GOTTY_PORT)+" -w "+tmux_executable+" a -t "+terminal_id)
     os.system(tmux_executable+" kill-session -t "+terminal_id)
 except KeyboardInterrupt as e:
