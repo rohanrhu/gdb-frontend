3fd888cbd55078272ac7487d23a56eb7c573087e
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2020-04-24 00:40:53 +0300

    Set version

--- statics.py
+++ statics.py
@@ -8,7 +8,7 @@
 # Licensed under GNU/GPLv3
 # Copyright (C) 2019, Oğuzhan Eroğlu (https://oguzhaneroglu.com/) <rohanrhu2@gmail.com>
 
-VERSION = [0, 0, 19, "git"]
+VERSION = [0, 0, 19, "alpha"]
 
 """
 GDB-Frontend GUI modes.
