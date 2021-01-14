d462a70caa1d1dbf9ef1eaa859b305eeb5a12612
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2021-01-14 23:36:18 +0300

    Dark theme added.

---
+++ plugins/theme_dark/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# gdb-frontend is a easy, flexible and extensionable gui debugger
+#
+# https://github.com/rohanrhu/gdb-frontend
+# https://oguzhaneroglu.com/projects/gdb-frontend/
+#
+# Licensed under GNU/GPLv3
+# Copyright (C) 2019, Oğuzhan Eroğlu (https://oguzhaneroglu.com/) <rohanrhu2@gmail.com>
+
+urls = {
+}
