4111a03f2e869ab34cd1468eac6ae57d2309e4b7
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2021-09-05 21:42:23 +0300

    Added Cyberpunk theme.

---
+++ plugins/theme_cyberpunk/theme_cyberpunk.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# gdb-frontend is a easy, flexible and extensionable gui debugger
+#
+# https://github.com/rohanrhu/gdb-frontend
+# https://oguzhaneroglu.com/projects/gdb-frontend/
+#
+# Licensed under GNU/GPLv3
+# Copyright (C) 2019, Oğuzhan Eroğlu (https://oguzhaneroglu.com/) <rohanrhu2@gmail.com>
+
+# Example GDBFrontend Plugin
+
+import importlib
+
+import plugin
+
+class ThemeCyberpunkPlugin(plugin.GDBFrontendPlugin):
+    def __init__(self):
+        plugin.GDBFrontendPlugin.__init__(self)
+
+    def loaded(self):
+        pass
+
+    def unloaded(self):
+        pass
