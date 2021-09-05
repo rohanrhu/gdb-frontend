12c5b9b9f192887a3f80dd8f6eec3c3561231497
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2021-09-05 17:52:04 +0300

    Fix automatic browser open for default port option

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ import time
 import re
 import signal
 import platform
+import webbrowser
 
 import config
 config.init()
@@ -354,6 +355,11 @@ try:
     if not is_random_port:
         print("Listening on %s: http://%s:%d/" % (config.BIND_ADDRESS, config.HOST_ADDRESS, config.HTTP_PORT))
         print(("Open this address in web browser: \033[0;32;40mhttp://%s:%d/terminal/\033[0m" % (config.HOST_ADDRESS, config.HTTP_PORT)))
+
+        gf_url = "http://%s:%d/terminal/" % (config.HOST_ADDRESS, config.HTTP_PORT)
+
+        if not webbrowser.open(gf_url):
+            os.system("/mnt/c/windows/system32/rundll32.exe url.dll,FileProtocolHandler %s" % gf_url)
         
         gotty.wait()
 
@@ -392,8 +398,6 @@ try:
 
         print("Listening on %s: http://%s:%d/" % (config.BIND_ADDRESS, config.HOST_ADDRESS, config.HTTP_PORT))
         print(("Open this address in web browser: \033[0;32;40mhttp://%s:%d/terminal/\033[0m" % (config.HOST_ADDRESS, config.HTTP_PORT)))
-
-        import webbrowser
 
         gf_url = "http://%s:%d/terminal/" % (config.HOST_ADDRESS, config.HTTP_PORT)
 
