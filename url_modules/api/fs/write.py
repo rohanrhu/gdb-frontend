9e97d3c1b2acd3d7a4902e361380127926b406e8
ksXV <msgcipi@gmail.com>
2024-12-27 11:24:19 +0200

    removed cgi - deprecated package

--- url_modules/api/fs/write.py
+++ url_modules/api/fs/write.py
@@ -7,11 +7,9 @@
 #
 # Licensed under GNU/GPLv3
 # Copyright (C) 2019, Oğuzhan Eroğlu (https://oguzhaneroglu.com/) <rohanrhu2@gmail.com>
-
 import json
 import os
 import urllib
-import cgi
 
 def run(request, params):
     if params is None: params = {}
@@ -26,21 +24,18 @@ def run(request, params):
     else:
         result_json["ok"] = True
 
-        form_data = cgi.FieldStorage(
-            fp=request.rfile,
-            headers=request.headers,
-            environ={"REQUEST_METHOD": "POST"}
-        )
+        content_length = int(request.headers.get('Content-Length', 0))
+        post_data: bytes = request.rfile.read(content_length)
 
-        file_path = form_data.getvalue("path")
+        form_data = urllib.parse.parse_qs(post_data.decode('utf-8'))
+        file_path = form_data["path"][0]
 
         is_exists = os.path.exists(file_path)
         is_dir = os.path.isdir(file_path) if is_exists else False
 
         if is_exists and not is_dir:
-            fd = open(file_path, 'w')
-            fd.write(form_data.getvalue("content"))
-            fd.close()
+            with open(file_path, 'w') as fd:
+                fd.write(form_data["content"][0])
         else:
             result_json["error"] = {
                 "not_exists": not is_exists,
